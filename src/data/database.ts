b1eb9e71b0e857022e1376d077c8c24585ae797b
dev progg <devnprogg@gmail.com>
2025-02-19 10:11:29 +0200

    creds update

--- src/data/database.ts
+++ src/data/database.ts
@@ -1,8 +1,8 @@
 import {createClient} from "@supabase/supabase-js"
 
 
-const URL = "https://efgmzfpesflputlviatp.supabase.co"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZ216ZnBlc2ZscHV0bHZpYXRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzM5NDg4MCwiZXhwIjoyMDQ4OTcwODgwfQ.puFXFL-zf3GCzt7SSZIpNIJA-j5NVz26zhyEmiphwmc"
+const URL = "https://uuzbangvjnascgpnadvl.supabase.co"
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1emJhbmd2am5hc2NncG5hZHZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTc3Mjc3MywiZXhwIjoyMDU1MzQ4NzczfQ.Klcbf0PQEUUnIaG7SvDHGt6pTnx7tCMYrap3_mirx0Q"
 
 export const supabase = createClient(URL, key);
 
